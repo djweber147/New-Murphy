0e2d6d907c5db538bfd07040bea86673569802da
clem0051 <31710421+clem0051@users.noreply.github.com>
2018-11-20 16:46:32 -0600

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -4,9 +4,34 @@ const term = 20; // 10 - Jterm, 20 - Spring, 30 - Summer, 40 - Fall
 
 
 // Requires
+var path = require('path');
+var express = require('express');
 const rp = require('request-promise');
 const $ = require('cheerio');
+const r = require('request');
+const sqlite3 = require('sqlite3');
 
+// Create Database
+var db  = new sqlite3.Database(path.join(__dirname, 'db', 'newMurphy.sqlite3'), (err) => 
+{
+  if (err)
+  {
+    console.log('Error Opening Database');
+  }
+  else
+  {
+    console.log('Connected to Database');
+    db.run("CREATE TABLE departments (subject TEXT PRIMARY KEY NULL, full_name TEXT NULL)");
+
+    db.run("CREATE TABLE courses (subject TEXT, course_number INTEGER, credits INTEGER, name TEXT, description TEXT)");
+
+    db.run("CREATE TABLE sections (crn INTEGER PRIMARY KEY NULL, subject TEXT NULL, course_number INTEGER NULL, section_number INTEGER NULL, building TEXT NULL, room TEXT NULL, professors TEXT NULL, times TEXT NULL, capacity INTEGER NULL, registered TEXT NULL)");
+
+    db.run("CREATE TABLE people (university_id INTEGER PRIMARY KEY NULL, position TEXT NULL, password TEXT NULL, first_name TEXT NULL, last_name TEXT NULL, registered_courses TEXT NULL)");
+
+    console.log("Created tables")
+  }
+});
 
 // Get Subject List
 var url = "https://classes.aws.stthomas.edu/index.htm?year=" + year + "&term=" + term + "&schoolCode=ALL&levelCode=ALL&selectedSubjects=";
@@ -105,13 +130,17 @@ String.prototype.trim = function() {
 // Add to database functions
 function addSubject(subject, full_name) {
     console.log("ADDED SUBJECT: " + subject + " - " + full_name);
+    db.run("INSERT INTO departments (subject, full_name) VALUES (? , ?)", subject, full_name);
 }
 
 function addCourse(subject, course_number, credits, name, description) {
     console.log("ADDED COURSE: " + subject + course_number + ": " + name + " " + credits + " credits. ");//+ description);
+    db.run("INSERT INTO courses (subject, course_number, credits, name, description) VALUES (? , ?, ?, ? ,?)", subject, course_number, credits, name, description);
 }
 
 function addSection(crn, subject, course_number, section_number, building, room, professors, times, capacity) {
     console.log("ADDED SECTION: " + crn + "-" + subject + course_number + "-" + section_number + ": " + building + room + " " + professors + " " + times + " " + capacity);
     // In addition to parameters, add "registered" field
+    registered = "--";
+    db.run("INSERT INTO sections (crn, subject, course_number, section_number, building, room, professors, times, capacity, registered) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", crn, subject, course_number, section_number, building, room, professors, times, capacity, registered)
 }

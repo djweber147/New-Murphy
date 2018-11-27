965ca1bb942ee5308c00f522a599bd00fe8e2005
Douglas Weber <webe0119@stthomas.edu>
2018-11-27 15:45:05 -0600

    Add user functionality

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // Imports
-var express = require("express");
-var bodyParser = require("body-parser");
+var sqlite3 = require('sqlite3');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 // Set up POST processing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Database
+var db = new sqlite3.Database(path.join(__dirname, 'db', 'newMurphy.sqlite3'), (err) => {
+    if (err) {
+        console.log('Error opening newMurphy.sqlite3 database');
+    }
+    else {
+        console.log('Now connected to newMurphy.sqlite3 database!');
+    }
+});
 
 // Set up public directory
 app.use(express.static('public'));
@@ -24,6 +36,26 @@ app.post('/login', function(req, res) {
     res.end("done");
 });
 
+// Process New User
+app.post('/newuser', function(req, res) {
+     var username = req.body.username;
+     var password = req.body.password;
+     var position = req.body.position;
+     var fname = req.body.fname;
+     var lname = req.body.lname;
+     
+     // Could use more error checking along the way (values can't be '')
+     db.run("INSERT INTO people VALUES (?,?,?,?,?,NULL);", username, position, password, fname, lname, function(err) {
+         if(err) {
+            console.log(err);
+         }
+     });
+         
+     // Check Login and send back "done"
+     res.end("done");
+});
+
+// Start Server
 app.listen(8005, function() {
     console.log("Started on PORT 8005");
 });

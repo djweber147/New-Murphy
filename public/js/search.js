1cc152e29027791b1de19dbe7da2f79acef26886
clem0051 <31710421+clem0051@users.noreply.github.com>
2018-11-29 22:38:06 -0600

    Update search.js

--- public/js/search.js
+++ public/js/search.js
@@ -8,18 +8,30 @@ $(document).ready(function() {
 		$.post("searchdept",function(data){
 			var result = JSON.parse(data);
 			for(var i = 0; i < result.length; i++){
-			var a = "<input type='checkbox' id='" + result[i].subject + "'>" + result[i].subject + "</input>";
-			$("#department2").append(a);
+console.log(result[i].subject);
+			var a = "<input type='checkbox' id='" + result[i].subject + "'> " + result[i].full_name + " </input>";
+			$("#departments2").append(a);
 			}
 		});
-		deptLoaded = true;
+		deptLoaded = true;	
 	}
      $("#submit").click(function() {
-         var crn = $("#crn").val();
-         var coursenumber = $("#coursenumber").val();
-         var departments = $("#departments").val();
+         var crn = $("#crn").val() || null;
+         var coursenumber = $("#coursenumber").val()|| null;
+		 var departments = []; // Array of departments
+		 $( "input" ).each(function(index){
+			 if (this.type === 'checkbox' && this.checked === true){
+				departments.push(this.id);
+			 }
+		 });
         $.post("courses", {crn: crn,coursenumber: coursenumber,departments: departments}, function(data) {
              console.log(data);
+	     var result = JSON.parse(data);
+			for(var i = 0; i < result.length; i++){
+				var a = "<tr><td>"+result[i].subject +"</td><td>"+result[i].course_number+ "-" +result[i].section_number+"</td><td>"+result[i].building +result[i].room +"</td><td>"+result[i].crn +"</td> </tr>";
+				$("#results").append(a);
+				//$("#results").style.hidden = false;
+			}
 //             console.log($('#errormessage').text());
 //             if (data === 'done') {
 //				 window.location.replace("search.html");
